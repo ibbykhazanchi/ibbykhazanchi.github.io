9f232e48e65f0b0e91eafb4233248f47935a1030
Ibby Khajanchi <khajanchi.ibrahim@gmail.com>
2024-05-04 18:09:32 -0400

    use hashrouter instead

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import BillyLogo from './pages/assets/BillyLogo';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App/>
