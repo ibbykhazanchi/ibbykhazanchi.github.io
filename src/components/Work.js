f6699e3c63dd9f371b6443b6c0dd7318a78834d1
Ibby Khajanchi <khajanchi.ibrahim@gmail.com>
2024-05-04 15:42:32 -0400

    add billy

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,9 +7,16 @@ const Work = () => {
         <div>
             <h4 className="display-4 fw-bold pt-5"> stuff i've done </h4>
             <h4 className='p-1'> 
-                <StyledLink href="https://chromewebstore.google.com/detail/notate/iblgligkiffmjapcipbaiogjfhnlaako?hl=en"> 
-                <b> notate</b>, annotate any webpage
-                </StyledLink>
+                <ul style={{listStyle: "none"}}>
+                    <li>
+                        <StyledLink href="https://chromewebstore.google.com/detail/notate/iblgligkiffmjapcipbaiogjfhnlaako?hl=en"> 
+                            <b> notate</b>, annotate any webpage
+                        </StyledLink>
+                    </li>
+                    <li>
+                        <b> billy</b>, stealth
+                    </li>
+                </ul>
             </h4>
         </div>
     )
